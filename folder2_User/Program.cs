069f322b0ea46438228b255e8516e7d0b70e349a
Dzambulov Arman <arman1407@ya.ru>
2023-05-20 13:32:02 +0300

    folder2_User

---
+++ folder2_User/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введие ваше имя");
+        string username = Console.ReadLine();
+        Console.Write("Привет, ");
+        Console.Write(username);
+    }
+}
