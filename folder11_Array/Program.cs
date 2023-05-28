15879f4309df34cfa4f15ebf6e41fe203a66b81a
Dzambulov Arman <arman1407@ya.ru>
2023-05-28 22:11:25 +0300

    folder11_array

---
+++ folder11_Array/Program.cs
@@ -0,0 +1,54 @@
+﻿// Заполняет массив случайными цифрами в указанном диапозоне (1-10)
+// void - метод который не возврашает
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length; // длина массива
+    int index = 0;
+    while (index < length) 
+    {
+        collection[index] = new Random().Next(1, 10); // новое случайное число из диапозона от 1 до 10
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int[10]; // создай новый массив с 10 элементам
+
+FillArray(array);   // наименование массива
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array);  // распечатывает массив
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
+
