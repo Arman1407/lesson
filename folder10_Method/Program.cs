2a87334c787cf819f4d37dd2f819c50144636de4
Dzambulov Arman <arman1407@ya.ru>
2023-05-28 21:27:18 +0300

    folder10_ethod

---
+++ folder10_Method/Program.cs
@@ -0,0 +1,18 @@
+﻿// Определение номера элемента по цифре в массиве
+
+int[] array = {12, 26, 65, 45, 54, 68, 45, 65, 85};
+
+int n = array.Length; //возвращает количество эллементов массива
+int find = 45;   // цифра в массиве
+
+int index = 0;  // начинаем с 1-го элемента
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+index++;
+}
