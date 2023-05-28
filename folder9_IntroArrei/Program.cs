226f8e44990c0aeea3440a4abf9e2fce637cf486
Dzambulov Arman <arman1407@ya.ru>
2023-05-28 21:05:37 +0300

    folder9_introarrey

--- folder9_IntroArrei/Program.cs
+++ folder9_IntroArrei/Program.cs
@@ -8,16 +8,18 @@ int Max(int arg1, int arg2, int arg3)
     return result;
 } //           0    1    2   3   4   5    6   7     8
 int[] array ={12, 5265, 54, 25, 52, 654, 54, 657, 8563 };
-
-int max = Max(
-    Max(a1, b1, c1), 
-    Max(a2, b2, c2), 
-    Max(a3, b3, c3));
-
-Console.WriteLine(max);
+// array[0] = 6;               // обращается к массиву и записывает в него значение
+// Console.WriteLine(array[1]); // обращается к массиву и получить из него значение по указанному индексу
 
 
+int result = Max
+(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
 
+Console.WriteLine(result);
 
 
 
